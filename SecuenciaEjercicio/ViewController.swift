5ca0aac0fcf9cdcb53220dac9ef78eae96062d43
Alumno <ulsa@iMacD114.local>
2019-08-20 14:37:21 -0700

    Secuencia, detener y reproducir

--- SecuenciaEjercicio/ViewController.swift
+++ SecuenciaEjercicio/ViewController.swift
@@ -9,10 +9,38 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    
+    
+    @IBOutlet weak var imgSecuencia: UIImageView!
+    
+    @IBAction func doTapConejo(_ sender: Any) {
+        if imgSecuencia.isAnimating{
+            
+            imgSecuencia.startAnimating()
+            
+        } else {
+            
+                imgSecuencia.stopAnimating()
+            
+        }
 
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        //
+        var imagenesSecuenciaConejo : [UIImage] = []
+        imagenesSecuenciaConejo.append(UIImage(named:"conejo1")!)
+        imagenesSecuenciaConejo.append(UIImage(named:"conejo2")!)
+        imagenesSecuenciaConejo.append(UIImage(named:"conejo3")!)
+        imagenesSecuenciaConejo.append(UIImage(named:"conejo4")!)
+        imagenesSecuenciaConejo.append(UIImage(named:"conejo5")!)
+        imagenesSecuenciaConejo.append(UIImage(named:"conejo6")!)
+        imagenesSecuenciaConejo.append(UIImage(named:"conejo7")!)
+
+        imgSecuencia.animationImages = imagenesSecuenciaConejo
+        imgSecuencia.animationDuration = 0.6
     }
 
     override func didReceiveMemoryWarning() {
